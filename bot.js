c0e4c73af00ecdeb5c02bd2b4a78819977c3745c
adam clark <aclark64@gmail.com>
2016-02-17 15:51:24 -0700

    1st

---
+++ bot.js
@@ -0,0 +1,29 @@
+var Twit = require('twit');
+
+var T = new Twit(require('./config.js'));
+
+var nationalParkSearch = {q: "national park filter:images", count: 10, result_type: "recent"};
+
+function retweetLatest() {
+	T.get('search/tweets', nationalParkSearch, function (error, data) {
+	  if (!error) {
+		var retweetId = data.statuses[0].id_str;
+		data.statuses[0].favorited = true;
+		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+			if (response) {
+				console.log(data.statuses[0]);
+				console.log('Success! Check your bot, it should have retweeted something.')
+			}
+			if (error) {
+				console.log('There was an error with Twitter:', error);
+			}
+		})
+	  }
+	  else {
+	  	console.log('There was an error with your hashtag search:', error);
+	  }
+	});
+}
+
+retweetLatest();
+setInterval(retweetLatest, 1000 * 60 * 60);
